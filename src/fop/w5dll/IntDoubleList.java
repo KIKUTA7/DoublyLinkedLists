93094ee5ea9599220587f6c7102e3ef8e935d9d7
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-05 16:42:25 +0400

    pirveli subtaski.

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -1,13 +1,42 @@
 package fop.w5dll;
 
 public class IntDoubleList {
-	private int info;
+	private double info;
 	public IntDoubleList next;
-	public IntDoubleList prev;
-	public IntDoubleList (int x)
+    public IntDoubleList prev;
+    public IntDoubleList (double x)
     {
         this.info = x;
-        this.prev = this;
         this.next = this;
     }
+    public IntDoubleList ()
+    {
+
+        if(this.prev != null)
+            this.next =null;
+    }
+    public IntDoubleList getPrev() {
+        return prev;
+    }
+
+    public IntDoubleList getNext() {
+        return next;
+    }
+
+    public double getInfo() {
+        return info;
+    }
+
+    public void setPrev(IntDoubleList prev) {
+        this.prev = prev;
+    }
+
+    public void setNext(IntDoubleList next) {
+        this.next = next;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
 }
