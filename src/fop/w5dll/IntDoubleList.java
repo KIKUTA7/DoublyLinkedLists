a48e943964f5b2c5eefa2735d53dc3d1d70b4283
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-08 15:53:24 +0400

    raghaca mainc

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -3,5 +3,13 @@ package fop.w5dll;
 public class IntDoubleList {
     private IntDoubleListElement head;
     private IntDoubleListElement tail;
+    public IntDoubleListElement getFirstElement()
+    {
+        return this.head;
+    }
+    public IntDoubleListElement getLastElement()
+    {
+        return this.tail;
+    }
 }
 
