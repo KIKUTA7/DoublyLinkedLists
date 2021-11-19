87cadeb4a302f042fd1fada573d524478a398163
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-19 14:09:51 +0400

    exla ukve 100 ze gvinda

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -101,7 +101,7 @@ public class IntDoubleList {
     {
         int k=0;
         IntDoubleListElement [] x= new IntDoubleListElement[size()];
-        for (IntDoubleListElement t=head;t.next!=null;t=t.next)
+        for (IntDoubleListElement t=head;t!=null;t=t.next)
         {
             if(t.getInfo() == intValue && this.head ==this.tail) {x[k]= t;x[k].prev=null;x[k].next = null;k++;}
             else if(t.getInfo() == intValue ) {
